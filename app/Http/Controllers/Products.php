d299c23a9c5044607fbcef7ac5cd447a4d1f0dd4
PrajwolK <PrajwolK09@gmail.com>
2024-05-05 16:06:10 +0545

    feature: api to get data by id

--- app/Http/Controllers/Products.php
+++ app/Http/Controllers/Products.php
@@ -8,14 +8,28 @@ use Illuminate\Support\Facades\DB;
 class Products extends Controller
 {
     public function getAllProducts(Request $request){
-        $data = DB::table('products')->get()->toArray();
+        $data = DB::table('products')->select('*')->get()->toArray();
         if(empty($data)){
             return response()->json([
                 'message' => 'No products available in your database'
-            ]);
+            ],500);
         }
         return $data;
     }
-
+    public function getProductByName(Request $request){
+        $name = $request->input('product_name');
+        $data = DB::table('products')->where("name","like","%$name%")->get()->toArray();
+        if(empty($data)){
+            return response()->json([
+                'message' => 'No product found with the given name'
+            ],500);
+        }
+        return $data;
+    }
+    public  function getById(Request $request){
+        $id = (int)$request->input('id');
+        $data = DB::table('products')->where('id',$id)->get();
+        return response()->json($data,200);
+    }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::group(['prefix'=>'products'],function(){
-    Route::get('/all',[ProductController::class,'getAll']);
+    Route::get('/all',[ProductController::class,'getAllProducts']);
+    Route::get('name/{product_name}',[ProductController::class,'getProductByName']);   
+    Route::get('/{id}',[ProductController::class,'getById']);
 });
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
