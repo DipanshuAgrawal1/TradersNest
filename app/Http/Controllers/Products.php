f535d95e8e1cbffcd509a65ad0be006e11f30e1f
PrajwolK <PrajwolK09@gmail.com>
2024-05-05 15:51:59 +0545

    fix: error handling for all products api

--- app/Http/Controllers/Products.php
+++ app/Http/Controllers/Products.php
@@ -8,7 +8,14 @@ use Illuminate\Support\Facades\DB;
 class Products extends Controller
 {
     public function getAllProducts(Request $request){
-        $data = DB::table('products')->get();
+        $data = DB::table('products')->get()->toArray();
+        if(empty($data)){
+            return response()->json([
+                'message' => 'No products available in your database'
+            ]);
+        }
         return $data;
     }
+
 }
+
